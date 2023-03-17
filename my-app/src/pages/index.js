1816eb9ba1251388bed5b6ba2d2b1d8dfdd5eff1
MonicaCodreanu <monica.codreanu@gmail.com>
2023-03-17 10:40:10 +0100

    Added more recipes

--- my-app/src/pages/index.js
+++ my-app/src/pages/index.js
@@ -7,24 +7,58 @@ import RecipeCard from "./recipeCard";
 
 export default function Main() {
   const [recipes, setRecipes] = useState([]);
-  const handleCardClick = (e) => {};
+  const [textInput, setTextInput] = useState("");
+  const [filteredRecipes, setFilteredRecipes] = useState([]);
+
+  const alpha = Array.from(Array(26)).map((e, i) => i + 65);
+  const alphabet = alpha.map((x) => String.fromCharCode(x));
+
   const fetchData = () => {
-    fetch("https://www.themealdb.com/api/json/v1/1/search.php?f=a")
-      .then((res) => res.json())
-      .then((data) => {
-        setRecipes(data.meals);
-      });
+    alphabet.forEach((letter) =>
+      fetch(
+        `https://www.themealdb.com/api/json/v1/1/search.php?f=${letter.toLowerCase()}`
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.meals && Array.isArray(data.meals)) {
+            setRecipes((prev) => [...prev, ...data.meals]);
+            setFilteredRecipes((prev) => [...prev, ...data.meals]);
+          } else if (data.meals) {
+            setRecipes((prev) => [...prev, data.meals]);
+            setFilteredRecipes((prev) => [...prev, data.meals]);
+          }
+        })
+    );
+  };
+  useEffect(fetchData, []);
+  // const shuffledRecipes = recipes.sort((a, b) => 0.5 - Math.random());
+
+  // setFilteredRecipes(shuffledRecipes);
+  const handleChange = (e) => {
+    setTextInput(e.target.value);
   };
 
-  useEffect(fetchData, []);
+  useEffect(() => {
+    if (textInput) {
+      const filtered = filteredRecipes.filter((recipe) => {
+        return recipe.strMeal.toLowerCase().includes(textInput.toLowerCase());
+      });
+      setFilteredRecipes(filtered);
+    } else {
+      setFilteredRecipes(recipes);
+    }
+  }, [textInput]);
+
   return (
     <>
       <h1 className="header">Laga mat med MAJA. M</h1>
+      <input type="text" placeholder="Search recipe" onChange={handleChange} />
+
       <main className="wrapper">
-        {recipes.length &&
-          recipes.map((recipe) => (
+        {filteredRecipes.length &&
+          filteredRecipes.map((recipe, index) => (
             <RecipeCard
-              key={recipe.idMeal}
+              key={index}
               name={recipe.strMeal}
               img={recipe.strMealThumb}
               id={recipe.idMeal}
